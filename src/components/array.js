a0a1c51267ce49bd0fb405794f5c3bd4fd3c910d
ali <syedaliibrahim50@gmail.com>
2022-11-29 17:46:01 +0500

    List And Keys

--- src/components/array.js
+++ src/components/array.js
@@ -1,34 +1,40 @@
 import React from 'react'
 import Student from './student';
 function App() {
-    const Students=["jk","fff","okok","kk"];
-    let studentName = Students.map(std=><li>{std}</li>);
+    // const Students=["jk","fff","okok","kk"];
+    // let studentName = Students.map((std,index)=> <li key={index}>{std}</li>);
     // const num =[1,2,4,5,]
     // const result =  num.map(nu=> <h1>{ nu = nu * 2 }</h1>)
 
 
-    // const Students =[
-    //     {
-    //         name:"ali",
-    //         age:25,
-    //     },
-    //     {
-    //         name:"lkali",
-    //         age:25,
-    //     },
-    //     {
-    //         name:"muuiali",
-    //         age:25,
-    //     }
-    // ]
+    const Students =[
+        {
+            id:1,
+            name:"ali",
+            age:25,
+        },
+        {
+            id:2,
+            name:"lkali",
+            age:25,
+        },
+        {
+            id:3,
+            name:"muuiali",
+            age:25,
+        }
+    ]
 
 
     return (
       <div>
+
+ {/* {<ul>{studentName}</ul>} */}
+
         {/* {Students.map(std=> <h1>I am {std.name} and I am {std.age} years old</h1>)} */}
-        {/* {Students.map(std=> <Student std={std}/>)} */}
+        {Students.map(std=> <Student key={std.id} std={std}/>)}
         {/* {result} */}
-        {Students.map(std=> <h1>{std}</h1>)}
+        {/* {Students.map(std=> <h1>{std}</h1>)} */}
         {/* {num.map(nu=> <h1>{ nu = nu * 2 }</h1>)} */}
       </div>
     )
